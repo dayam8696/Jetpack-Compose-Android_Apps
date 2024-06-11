c5d803261ff325eaecbf1ec723417fe3a80d2f3f
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-11 22:45:59 +0530

    added recycleview

--- app/src/main/java/com/example/jetpack_compose_androidapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_androidapp/MainActivity.kt
@@ -1,26 +1,130 @@
 package com.example.jetpack_compose_androidapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Text(text = "Hello Dayam")
+UseCard()
 
         }
     }
 }
-@Preview(showBackground = true , name = "Hello msg", showSystemUi = true)
+
 @Composable
-fun Dayam(name:String = "Cool Dayam"){
-    Text(text = "Hello $name")
+fun UseCard() {
+    Card(
+        elevation = CardDefaults.cardElevation(4.dp),
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentWidth()
+
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth()
+                .padding(12.dp)
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+
+            Column(
+
+            ) {
+
+                Text(text = stringResource(id = R.string.dummy_text))
+                Button(onClick = {
+                    // perform button action here!
+                }) {
+                    Text(text = "View Profile")
+                }
+            }
+        }
+    }
+
+
 }
+
+@Composable
+fun Title() {
+    val context = LocalContext.current
+    Text(
+        text = "Dayam Nadeem",
+        fontSize = 32.sp,
+        color = colorResource(id = R.color.purple_200),
+        fontFamily = FontFamily.Cursive,
+        modifier = Modifier.clickable {
+            Toast.makeText(context, "Tittle Clicked", Toast.LENGTH_LONG).show()
+        }
+
+
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+    }
+
+    UseCard()
+
+}
+
+@Composable
+//list view
+fun UserList(){
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+        for (i in 1..10){
+            UseCard()
+        }
+
+    }
+}
+
 
