5c57f0eed9912eef2767e0fdbf5669b6f11e55e1
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-06-14 14:55:17 +0530

    added state

--- app/src/main/java/com/example/jetpack_compose_androidapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_androidapp/MainActivity.kt
@@ -22,7 +22,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -40,7 +43,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-UseCard()
+        TextInput()
 
         }
     }
@@ -125,6 +128,21 @@ fun UserList(){
         }
 
     }
+}
+
+@Composable
+fun TextInput(){
+    val state = remember {
+        mutableStateOf("")
+    }
+
+    TextField(value = state.value,
+        onValueChange ={
+            state.value = it
+        },
+        label = {Text(text = "Enter the message ")}
+        
+    )
 }
 
 
